44c223eb3c6ec563ef8e75754f4657a5a299cf05
unknown <pnei0001@student.monash.edu>
2019-02-14 23:35:02 +1100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//Patrick Neilson 2019
+//Project built following this video series by GeekLaunch: https://www.youtube.com/watch?v=vJdT05zl6jk
+//
+//A blockchain side project started in Feburary 2019
+//
+
+fn main() {
+    println!("Hello, world!");
+}
